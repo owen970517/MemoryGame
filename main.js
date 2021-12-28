ea8fb7c53101bd0bebebffaf6e8f1cfcede64498
Owen <tamia123123@naver.com>
2021-12-28 17:51:46 +0900

    second commit

--- main.js
+++ main.js
@@ -1,8 +1,19 @@
 const section = document.querySelector('section');
-const counts = document.querySelector('.playerLiveCounts');
-playerCounts = 6;
+const score1 = document.querySelector('.player1Score');
+const score2 = document.querySelector('.player2Score');
+const p1 = document.querySelector('.player1');
+const p2 = document.querySelector('.player2');
 
-counts.textContent = playerCounts;
+// true == player1 , false == player2 
+let isMyTurn = true;
+p1.classList.add('active');
+firstScore = 0;
+secondScore = 0;
+
+score1.textContent = firstScore;
+score2.textContent = secondScore;
+
+//counts.textContent = playerCounts;
 
 const getData = () => [
     {imgSrc : "./images/iu.jpg" , name : "iu" },
@@ -13,7 +24,7 @@ const getData = () => [
     {imgSrc : "./images/iu6.jpg" , name : "iu6" },
     {imgSrc : "./images/iu7.jpg" , name : "iu7" },
     {imgSrc : "./images/iu8.jpg" , name : "iu8" },
-    {imgSrc : "./images/iu.jpg" , name : "iu1" },
+    {imgSrc : "./images/iu.jpg" , name : "iu" },
     {imgSrc : "./images/iu2.jpg" , name : "iu2" },
     {imgSrc : "./images/iu3.jpg" , name : "iu3" },
     {imgSrc : "./images/iu4.jpg" , name : "iu4" },
@@ -21,12 +32,6 @@ const getData = () => [
     {imgSrc : "./images/iu6.jpg" , name : "iu6" },
     {imgSrc : "./images/iu7.jpg" , name : "iu7" },
     {imgSrc : "./images/iu8.jpg" , name : "iu8" },
-    //{imgSrc : "./images/iu9.jpg" , name : "iu9" },
-    //{imgSrc : "./images/iu10.jpg" , name : "iu10" },
-
-
-
-
 ]
 const randomize = () => {
     const cardData = getData();
@@ -36,13 +41,12 @@ const randomize = () => {
 
 const cardGenerator = () => {
     const cardData = randomize();
-    
     cardData.forEach(item => {
         const card = document.createElement('div');
         const face = document.createElement('img');
         const back = document.createElement('div');
         face.src = item.imgSrc;
-        card.setAttribute('name' , item.name);
+        card.setAttribute("name" , item.name);
         card.classList = 'card';
         face.classList = 'face';
         back.classList = 'back';
@@ -51,21 +55,47 @@ const cardGenerator = () => {
         card.appendChild(back);
         card.addEventListener('click' , (e)=> {
             card.classList.toggle('toggleCard');
-            checkCard(e);
-        })
-    })
-}
-const checkCard = (e) => {
-    const clickedCard = e.target;
-    clickedCard.classList.add('flipped');
-    const flippedCard = document.querySelectorAll('.flipped');
-    if (flippedCard.length === 2) {
-        if ( flippedCard[0].getAttribute('name') == flippedCard[1].getAttribute['name']) {
+            checkCards(e);
+        });
+    });
+};
+const checkCards = (e) => {
+    const clickedCard = e.target.offsetParent;
+    clickedCard.classList.add("flipped");
+    const flippedCards = document.querySelectorAll('.flipped');
+    if (flippedCards.length === 2 ) {
+        if ( flippedCards[0].getAttribute('name') === flippedCards[1].getAttribute('name')) {
             console.log('match');
+            if(isMyTurn) {
+               
+                firstScore+=100;
+                score1.textContent = firstScore;
+            } else {
+           
+                secondScore += 100;
+                score2.textContent = secondScore;
+            }
+            flippedCards.forEach(card => {
+                card.classList.remove('flipped');
+                card.style.pointerEvents = "none";
+            })
         } else {
             console.log('wrong');
+            isMyTurn = !isMyTurn;
+            if(isMyTurn == true) {
+                p1.classList.add('active');
+                p2.classList.remove('active');
+            } else {
+                p1.classList.remove('active');
+                p2.classList.add('active');
+            }
+           
+            flippedCards.forEach(card => {
+                card.classList.remove('flipped');
+                setTimeout(()=>card.classList.remove('toggleCard'),1000);
+            })
         }
     }
-
 }
+
 cardGenerator();
